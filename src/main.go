5713ecb0e487f0b487446791e9e8ab8f607c9d9f
킨 <69646118+Roflane@users.noreply.github.com>
2026-08-29 12:17:33 +0400

    basic http server

--- src/main.go
+++ src/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+
+	router.Get("/hello", basicHandler)
+
+	server := &http.Server{
+		Addr:    ":3000",
+		Handler: router,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func basicHandler(w http.ResponseWriter, _ *http.Request) {
+	_, _ = w.Write([]byte("Hello World"))
 }
